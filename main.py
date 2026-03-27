4cb8535a85b939028063579ee04d8aec461ae48c
Rishab Tiwari <your_github_email@gmail.com>
2026-03-27 09:11:55 +0530

    Fix FastAPI duplicate issue

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from agent import study_agent
+from fastapi.middleware.cors import CORSMiddleware
 
+# Create app ONCE
 app = FastAPI()
 
+# Enable CORS (for frontend connection)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Request model
 class Request(BaseModel):
     text: str
 
+# Health route
 @app.get("/")
 def home():
     return {"message": "AI Study Assistant is running 🚀"}
 
+# Main route
 @app.post("/study")
 def study(req: Request):
     return study_agent(req.text)
